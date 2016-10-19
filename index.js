436df641590c800e59f880a0f806c4118642e0f2
nqdinh <dinh.nguyen@veriksystems.com>
2016-10-19 22:50:08 +0700

    get and head work

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-let fs = require('fs').promise
+#!/usr/bin/env babel-node
+
+let fs = require('fs')
 let express = require('express')
-let PromiseRouter = require('express-promise-router')
+// let PromiseRouter = require('express-promise-router')
+// let trycatch = require('trycatch')
+// let bodyParser = require('body-parser')
 let morgan = require('morgan')
-let trycatch = require('trycatch')
-let bodyParser = require('body-parser')
+let nodeify = require('bluebird-nodeify')
+let path = require('path')
+let mime = require('mime-types')
+
 require('songbird')
+
+const NODE_ENV = process.env.NODE_ENV
+const PORT = process.env.PORT || 8000
+const ROOT_DIR = process.cwd()
 
 let app = express()
 
+// middleware
 app.use(morgan('dev'))
 
-app.get('*', (req, res) => {
-  console.log('get')
+function setHeaders(req, res, next) {
+  nodeify(async() => {
+    let contentPath = path.join(ROOT_DIR, req.url)
+    req.contentPath = contentPath
+    if (contentPath.indexOf(ROOT_DIR) !== 0) {
+      return res.send(400, 'Invalid path')
+    }
+    // res.setHeader('Content-Length', data.length)
+    let stat = await fs.promise.stat(contentPath)
+    req.stat = stat
+    next()
+  })().catch(next)
+}
+
+app.get('*', setHeaders, (req, res, next) => {
+  nodeify(async() => {
+    let stat = req.stat
+    let contentPath = req.contentPath
+
+    if (stat.isDirectory()) {
+      let files = await fs.promise.readdir(contentPath)
+      return res.json(files)
+    }
+    res.setHeader('Content-Type', mime.lookup(contentPath))
+    fs.createReadStream(contentPath).pipe(res)
+  })().catch(next)
 })
 
-app.head('*', (req, res) => {
-  console.log('head')
+app.head('*', setHeaders)
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}`)
 })
 
-app.listen(8000)
