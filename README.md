04bfdcc83a1264db9035cbfa959541f70734e008
nqdinh <dinh.nguyen@veriksystems.com>
2016-10-21 21:32:29 +0700

    del

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ Time spent: `<Number of hours spent>`
 - [x] Completed Requireds marked with `[x]`
 - [ ] Walkthrough Gif embedded in README
 - [ ] README `Time spent:` includes the number of hours spent on the assignment
-- [ ] Client can make GET requests to get file or directory contents
+- [x] Client can make GET requests to get file or directory contents
 - [ ] Client can download a directory as an archive
-- [ ] Client can make HEAD request to get just the GET headers 
-- [ ] Client can make PUT requests to create new directories and files with content
-- [ ] Client can make POST requests to update the contents of a file
+- [x] Client can make HEAD request to get just the GET headers 
+- [ ] Client can make ~~PUT~~ POST requests to create new directories and files with content
+- [ ] Client can make ~~POST~~ PUT requests to update the contents of a file
 - [ ] Client can make DELETE requests to delete files and folders
 - [ ] Server will serve from `--dir` or cwd as root
 - [ ] Server will sync `HTTP` modifications over TCP to the Client
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let morgan = require('morgan')
 let nodeify = require('bluebird-nodeify')
 let path = require('path')
 let mime = require('mime-types')
+let rimraf = require('rimraf')
 
 require('songbird')
 
@@ -21,18 +22,22 @@ let app = express()
 // middleware
 app.use(morgan('dev'))
 
+function setContentPath(req, res, next) {
+  let contentPath = path.join(ROOT_DIR, req.url)
+  req.contentPath = contentPath
+  next()
+}
+
 function setHeaders(req, res, next) {
   nodeify(async() => {
-    let contentPath = path.join(ROOT_DIR, req.url)
-    req.contentPath = contentPath
+    let contentPath = req.contentPath
     if (contentPath.indexOf(ROOT_DIR) !== 0) {
       return res.send(400, 'Invalid path')
     }
     let stat
     try {
       stat = await fs.promise.stat(contentPath)
-    }
-    catch(e) {
+    } catch (e) {
       return res.send(404, 'invalide path')
     }
     req.stat = stat
@@ -40,12 +45,13 @@ function setHeaders(req, res, next) {
     // since we stream the file, lets set the content length
     if (!stat.isDirectory()) {
       res.setHeader('Content-Length', stat.size)
+      res.setHeader('Content-Type', mime.lookup(contentPath))
     }
     next()
   })().catch(next)
 }
 
-app.get('*', setHeaders, (req, res, next) => {
+app.get('*', setContentPath, setHeaders, (req, res, next) => {
   nodeify(async() => {
     let stat = req.stat
     let contentPath = req.contentPath
@@ -54,12 +60,23 @@ app.get('*', setHeaders, (req, res, next) => {
       let files = await fs.promise.readdir(contentPath)
       return res.json(files)
     }
-    res.setHeader('Content-Type', mime.lookup(contentPath))
     fs.createReadStream(contentPath).pipe(res)
   })().catch(next)
 })
 
-app.head('*', setHeaders)
+app.head('*', setContentPath, setHeaders)
+
+app.delete('*', setContentPath, setHeaders, (req, res, next) => {
+  nodeify(async() => {
+    let stat = req.stat
+    if (stat.isDirectory()) {
+      await rimraf.promise(req.contentPath)
+    } else {
+      await fs.promise.unlink(req.contentPath)
+    }
+    return next()
+  })().catch(next)
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`)
