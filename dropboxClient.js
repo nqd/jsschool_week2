d17b6ad762f79f52a9e4e68e0827b7a9bc2f034b
nqdinh <dinh.nguyen@veriksystems.com>
2016-10-23 21:37:00 +0700

    forgot save package in package.json

--- dropboxClient.js
+++ dropboxClient.js
@@ -1,17 +1,33 @@
 
 let nssocket = require('nssocket')
+let url = require('url')
+let request = require('request')
+const ROOT_DIR = process.cwd()
+let argv = require('yargs').argv
+
+const rootDir = argv.dir || ROOT_DIR
 
 let outbound = new nssocket.NsSocket({
   reconnect: true,
   type: 'tcp4',
 });
 
-// outbound.data(['message', 'status'], (data) => {
-//   console.log(data);
-// })
-
 outbound.data(['message', 'sync'], (data) => {
   console.log(data);
 })
 
 outbound.connect(8001)
+
+// when bootup, download the root dir
+const hostname = 'localhost'
+const port = 8000
+
+let options = {
+  url: url.format({
+    protocol: 'http',
+    hostname: hostname,
+    port: port
+  }),
+  headers: { Accept: 'application/x-gtar' }
+}
+request(options, 'http://127.0.0.1:8000/').pipe(tarExtractStream)
